77a6ff02529fe3d57d430b460947648f7d32d27e
serhatbek <serhat.bekk@gmail.com>
2021-01-31 20:08:59 +0300

    swiper gallery

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  loop: true,
+  freeMode: true,
+  loopedSlides: 6, //looped slides should be the same
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  loop: true,
+  loopedSlides: 6, //looped slides should be the same
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
